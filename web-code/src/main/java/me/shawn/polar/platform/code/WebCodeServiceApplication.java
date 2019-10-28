331d7c73a9f82c701a6258740ddeaf6f6d5ca07a
SeHun Shin <s.shin@marathoner.kr>
2019-10-28 23:33:22 +0900

    feat: web-code-service 최초 커밋

---
+++ web-code/src/main/java/me/shawn/polar/platform/code/WebCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package me.shawn.polar.platform.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCodeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCodeServiceApplication.class, args);
+    }
+
+}
