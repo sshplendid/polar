9be5bfc47d48e7ee8d3efe4f7a045f24fc7464ec
SeHun Shin <s.shin@marathoner.kr>
2019-10-28 16:13:37 +0900

    chore: main application class

---
+++ mono-web/src/main/java/me/shawn/polar/mono/MonoWebApplication.java
@@ -0,0 +1,13 @@
+package me.shawn.polar.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonoWebApplication.class, args);
+    }
+
+}
