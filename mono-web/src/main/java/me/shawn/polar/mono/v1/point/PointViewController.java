95cbfb4765e61740d209fee6c5fd77a93e2e8c26
SeHun Shin <s.shin@marathoner.kr>
2019-11-11 18:13:29 +0900

    feat: 레디스 포인트 용 view 추가

---
+++ mono-web/src/main/java/me/shawn/polar/mono/v1/point/PointViewController.java
@@ -0,0 +1,24 @@
+package me.shawn.polar.mono.v1.point;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/points")
+public class PointViewController {
+
+    @Autowired
+    PointRedisRepository pointRedisRepository;
+
+    @GetMapping("/{id}")
+    public String getPointViewById(Model model, @PathVariable String id) {
+
+        Point point = pointRedisRepository.findById(id).orElse(Point.MOCK);
+        model.addAttribute("point", point);
+        return "/point/point";
+    }
+}
